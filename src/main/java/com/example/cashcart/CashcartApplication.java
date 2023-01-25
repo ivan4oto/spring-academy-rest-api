79ae542db6bac3cfe3b0c2a4913f9ff93b07016e
igochev <igochev@hedgeserv.com>
2023-01-25 11:42:27 +0200

    first commit spring initializr

---
+++ src/main/java/com/example/cashcart/CashcartApplication.java
@@ -0,0 +1,13 @@
+package com.example.cashcart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashcartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashcartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cashcart/CashcartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cashcart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashcartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
